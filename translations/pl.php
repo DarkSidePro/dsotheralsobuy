24b383a55af505d6768fa19b9a8fa2a1439a202e
Patryk Tchorzewski <patryk@dark-side.pro>
2021-12-09 21:25:41 +0000

    add polish translations

---
+++ translations/pl.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{dsothersalsobought}prestashop>displayfooterproduct_ee1d9a1453a91371908a01eb71c0ef78'] = 'Klienci, którzy kupili ten produkt, kupili również:';
+$_MODULE['<{dsothersalsobought}prestashop>displayfooterproduct_287c8697f7592ee492f5cad659d78941'] = 'Brutto';
